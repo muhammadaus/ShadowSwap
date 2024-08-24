847295af463af39c390cb8f1cbd24e529c2b9fed
MUHAMMAD AUS <103428274+muhammadaus@users.noreply.github.com>
2024-08-24 21:39:37 +0900

    replace hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,16 +5,16 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: `https://gateway-api.cabinet-node.com/0db81d915acae383cf7b91896d80ce9d`,
-      accounts: [`0x82550edf3152e9af8df21e3b301ee97d0c681be4b56951a58250d317fba0ed39`]
+      url: `REPLACE_WITH_APIKEY`,
+      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`]
     },
     scrollDevnet: {
       url: "https://l1sload-rpc.scroll.io",
-      accounts: [`0x82550edf3152e9af8df21e3b301ee97d0c681be4b56951a58250d317fba0ed39`]
+      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`]
     },
     scrollSepolia: {
-      url: "https://gateway-api.cabinet-node.com/b5f8d602629e60d5b3a1d6fa32ef8374",
-      accounts: [`0x82550edf3152e9af8df21e3b301ee97d0c681be4b56951a58250d317fba0ed39`]
+      url: "REPLACE_WITH_APIKEY",
+      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`]
     }
   },
   etherscan: {
--- scripts/deploy_token.js
+++ scripts/deploy_token.js
@@ -7,10 +7,21 @@
 const hre = require("hardhat");
 
 async function main() {
+  const Token = await hre.ethers.getContractFactory('Token')
+
+  // Deploy Token 1
+  let dapp = await Token.deploy('Dapp Token', 'DAPP', '1000000') // 1 million tokens
+  await dapp.deployed()
+  console.log(`Dapp Token deployed to: ${dapp.address}\n`)
+
+  // Deploy Token 2
+  const usd = await Token.deploy('USD Token', 'USD', '1000000') // 1 million tokens
+  await usd.deployed()
+  console.log(`USD Token deployed to: ${usd.address}\n`)
 
   // Deploy AMM
   const AMM = await hre.ethers.getContractFactory('AMM')
-  const amm = await AMM.deploy('0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238', '0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4')
+  const amm = await AMM.deploy(dapp.address, usd.address)
 
   console.log(`AMM contract deployed to: ${amm.address}\n`)
 
